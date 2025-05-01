b612f72c408627674edd436b6f9c4fff11d5a6e6
yantianyv <yantianyv@outlook.com>
2025-05-02 03:26:10 +0800

    修复了安装脚本异常的bug

--- build.sh
+++ build.sh
@@ -1,15 +1,22 @@
+#!/bin/bash
+
+# 检查并激活虚拟环境
 if [ -d "./venv" ]; then
     source ./venv/bin/activate
 else
     echo "正在创建虚拟环境..."
     python -m venv venv
     source ./venv/bin/activate
+    echo "正在安装依赖项，请确保网络通畅..."
     pip install -r requirements.txt
+fi
+
+# 使用 Nuitka 编译 Python 脚本
+echo "正在编译..."
 nuitka ./homepage.py \
-    --standalone\
-    --onefile\
+    --standalone \
+    --onefile \
     --include-data-dir=static=static \
     --include-data-dir=templates=templates \
     --output-dir=build \
     --remove-output
-
