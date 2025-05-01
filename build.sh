61b4645a3eb159a40c78f008809e62fff3216c6b
yantianyv <yantianyv@outlook.com>
2025-05-02 04:06:04 +0800

    现在编译后的二进制文件的名字会带上系统版本

--- build.sh
+++ build.sh
@@ -11,12 +11,21 @@ else
     pip install -r requirements.txt
 fi
 
+# 获取系统架构信息
+# 获取更详细的系统信息
+SYS_NAME=$(uname -s | tr '[:upper:]' '[:lower:]')
+ARCH=$(uname -m)
+OUTPUT_NAME="homepage_${SYS_NAME}_${ARCH}.bin"
+
 # 使用 Nuitka 编译 Python 脚本
 echo "正在编译..."
 nuitka ./homepage.py \
     --standalone \
     --onefile \
+    --jobs=$(nproc) \
+    --lto=yes \
     --include-data-dir=static=static \
     --include-data-dir=templates=templates \
+    --output-filename=${OUTPUT_NAME} \
     --output-dir=build \
     --remove-output
