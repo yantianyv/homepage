06ebc128405e8f9dddcb6d68bc668969d7812d6b
yantianyv <yantianyv@outlook.com>
2025-05-01 23:47:08 +0800

    添加了基于nuitka的打包脚本

---
+++ build.sh
@@ -0,0 +1,8 @@
+nuitka ./run.py \
+    --standalone\
+    --onefile\
+    --include-data-dir=static=static \
+    --include-data-dir=templates=templates \
+    --output-dir=build \
+    --remove-output
+
--- run.py
+++ run.py
@@ -11,7 +11,7 @@ from scripts import service_icons
 
 service_icons.refresh()
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static", template_folder="templates")
 app.secret_key = os.urandom(24)
 
 
